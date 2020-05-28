9c255cdba5c66d873b0dbdea5da81ced018780a9
Amanjain999 <amanjain@betalectic.com>
2020-05-28 13:25:12 +0530

    changes

--- resources/views/Post/edit_post.blade.php
+++ resources/views/Post/edit_post.blade.php
@@ -27,8 +27,8 @@
                             <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Content') }}</label>
 
                             <div class="col-md-8">
-                                <textarea id="body" type="textarea" class="form-control @error('content') is-invalid @enderror" name="content" value="{{$post->content}}" required
-                                rows='4' cols='5' autocomplete="body" autofocus  > </textarea>
+                                <textarea  id="content"  class="form-control @error('content') is-invalid @enderror" name="content" value="{{ $post->content }}" required]
+                                rows='4' cols='5' autocomplete="content" autofocus  > </textarea>
 
                                 @error('content')
                                     <span class="invalid-feedback" role="alert">
